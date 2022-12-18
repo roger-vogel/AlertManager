c719f448cbf89db5df2d62fc024040b63ebe9611
Roger Vogel <roger@puldypartners.com>
2022-12-18 12:23:46 -0800

    Initial Commit

---
+++ Sources/AlertManager/AlertManager.swift
@@ -0,0 +1,6 @@
+public struct AlertManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AlertManagerTests/AlertManagerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AlertManager
+
+final class AlertManagerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AlertManager().text, "Hello, World!")
+    }
+}
